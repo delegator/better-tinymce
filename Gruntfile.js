04ab3b9f80a081c715f8d70e2d9edce3f6f8c71f
Maureen Basil <Maureen.B@delegator.com>
2015-01-05 14:01:37 -0500

    Adds grunt to the module.

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'skin/frontend/base/default/css/styles.css' : 'skin/frontend/base/default/sass/styles.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'skin/frontend/base/default/sass/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
